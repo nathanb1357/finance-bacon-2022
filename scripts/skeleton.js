b9e3076fd757def3e85f36082b493377ec31d4de
Tyraffe <nathanb1357@gmail.com>
2022-10-26 10:30:01 -0700

    Added template.html and navbar template

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -3,7 +3,7 @@
 // (navbar, footer, and other things) into html doc. 
 //---------------------------------------------------
 function loadSkeleton(){
-    console.log($('#navbarPlaceholder').load('./text/nav.html'));
+    console.log($('#navbarPlaceholder').load('./text/navbar.html'));
     console.log($('#footerPlaceholder').load('./text/footer.html'));
 }
 loadSkeleton();  //invoke the function
