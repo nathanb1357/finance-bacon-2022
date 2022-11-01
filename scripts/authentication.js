3f7b13538f5ca693696b5085bcde497ca8a50bd4
Tyraffe <nathanb1357@gmail.com>
2022-11-01 13:04:38 -0700

    Creates user object when a new user logs in

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -3,18 +3,37 @@ var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
 var uiConfig = {
     callbacks: {
-      signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-        // User successfully signed in.
-        // Return type determines whether we continue the redirect automatically
-        // or whether we leave that to developer to handle.
-        return true;
-      },
+      signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+        // If the user is a "brand new" user, then create a new "user" in your own database.
+        // Assign this user with the name and email provided.
+        // Before this works, you must enable "Firestore" from the firebase console.
+        // The Firestore rules must allow the user to write. 
+        var user = authResult.user;                            // get the user object from the Firebase authentication database
+        if (authResult.additionalUserInfo.isNewUser) {         //if new user
+            db.collection("users").doc(user.uid).set({         //write to firestore. We are using the UID for the ID in users collection
+                    name: user.displayName,                    //"users" collection
+                    email: user.email,                         //with authenticated user's ID (user.uid)
+                    country: "Canada",                         //optional default profile info      
+                    school: "BCIT"                             //optional default profile info
+                }).then(function () {
+                    console.log("New user added to firestore");
+                    window.location.assign("main.html");       //re-direct to main.html after signup
+                })
+                .catch(function (error) {
+                    console.log("Error adding new user: " + error);
+                });
+        } else {
+            return true;
+        }
+        return false;
+    },
+
+      // Hide the loader.
       uiShown: function() {
-        // The widget is rendered.
-        // Hide the loader.
         document.getElementById('loader').style.display = 'none';
       }
     },
+
     // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
     signInFlow: 'popup',
     signInSuccessUrl: "home.html",
