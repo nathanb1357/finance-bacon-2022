c4fd56bc0d638863d9ef3796797ac8056278c734
Tyraffe <nathanb1357@gmail.com>
2022-11-03 11:24:58 -0700

    Added new Firebase API

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -11,10 +11,8 @@ var uiConfig = {
         var user = authResult.user;                            // get the user object from the Firebase authentication database
         if (authResult.additionalUserInfo.isNewUser) {         //if new user
             db.collection("users").doc(user.uid).set({         //write to firestore. We are using the UID for the ID in users collection
-                    name: user.displayName,                    //"users" collection
-                    email: user.email,                         //with authenticated user's ID (user.uid)
-                    country: "Canada",                         //optional default profile info      
-                    school: "BCIT"                             //optional default profile info
+                    name: user.displayName,                  
+                    email: user.email,                   
                 }).then(function () {
                     console.log("New user added to firestore");
                     window.location.assign("main.html");       //re-direct to main.html after signup
--- scripts/firebaseAPI_TEAM14.js
+++ scripts/firebaseAPI_TEAM14.js
@@ -1,14 +1,14 @@
 //----------------------------------------
 //  Your web app's Firebase configuration
 //----------------------------------------
-var firebaseConfig = {
-    apiKey: "AIzaSyAPCMbATa2_Wdla2DeFC--fRrqov8tdeQg",
-    authDomain: "comp1800-66495.firebaseapp.com",
-    projectId: "comp1800-66495",
-    storageBucket: "comp1800-66495.appspot.com",
-    messagingSenderId: "296263501210",
-    appId: "1:296263501210:web:4dbdeb790866268a442623"
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyAdqeYpumb0TyKqikskSmEc8cJS0bX2ebo",
+    authDomain: "finance-bacon.firebaseapp.com",
+    projectId: "finance-bacon",
+    storageBucket: "finance-bacon.appspot.com",
+    messagingSenderId: "343539326390",
+    appId: "1:343539326390:web:b5eab4ed76f820cc7e3517"
+  };
 
 //--------------------------------------------
 // initialize the Firebase app
