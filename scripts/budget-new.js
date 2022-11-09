9681dda9819c1c83a6204b19b2c5c1174af4b082
Tyraffe <nathanb1357@gmail.com>
2022-11-09 11:35:06 -0800

    Edited budget-new.js

--- scripts/budget-new.js
+++ scripts/budget-new.js
@@ -20,19 +20,22 @@ addBudget();
 function getCategories() {
     let budgetTemplate = document.getElementById("budget-template");
     let budgetGroup = document.getElementById("budget-group");
-    
-    db.collection("users").document(user.uid).collection("budget").get()
-        .then(allCategories => {
-            allCategories.forEach(doc => {
-                var categoryName = doc.data().name; //gets the name field
-                var categoryID = doc.data().ID; //gets the unique ID field
-                var categoryPercentage = doc.data().percentage; //gets the length field
-                let budgetRow = budgetTemplate.content.cloneNode(true);
-                budgetRow.querySelector('.category-name').innerHTML = categoryName;   //equiv getElementByClassName
-                budgetRow.querySelector('.category-percent').innerHTML = categoryPercentage;  //equiv getElementByClassName
-                budgetGroup.appendChild(budgetRow);
-            })
+    firebase.auth().onAuthStateChanged(user => {
+        // Check if a user is signed in:
+        if (user) {
+            currentUser.collection("budget").get()
+                .then(allCategories => {
+                    allCategories.forEach(doc => {
+                        var categoryName = doc.data().name; //gets the name field
 
-        })
+                        var categoryPercentage = doc.data().percentage; //gets the length field
+                        let budgetRow = budgetTemplate.content.cloneNode(true);
+                        budgetRow.querySelector('.category-name').innerHTML = categoryName;   //equiv getElementByClassName
+                        budgetRow.querySelector('.category-percent').innerHTML = categoryPercentage;  //equiv getElementByClassName
+                        budgetGroup.appendChild(budgetRow);
+                    });
+                });
+            };
+    });
 }
 getCategories();
