a7f5df122f08d9ff331bcb9d8435496a58983cf6
Tyraffe <nathanb1357@gmail.com>
2022-11-09 11:51:38 -0800

    Budget-new now autogenerates categories.

--- scripts/budget-new.js
+++ scripts/budget-new.js
@@ -1,14 +1,17 @@
+var currentUser;
+
 function addBudget(){
     firebase.auth().onAuthStateChanged(user => {
         // Check if a user is signed in:
         if (user) {
-            var currentUser = db.collection("users").doc(user.uid);
+            currentUser = db.collection("users").doc(user.uid);
             currentUser.collection("budget").doc("housing").set({
                 name: "Housing",
                 percentage: 0.35
             }, {merge: true})    
             .then(() => {
                 console.log("Document succesfully written!");
+                getCategories();
             });
         }
     })
@@ -20,22 +23,15 @@ addBudget();
 function getCategories() {
     let budgetTemplate = document.getElementById("budget-template");
     let budgetGroup = document.getElementById("budget-group");
-    firebase.auth().onAuthStateChanged(user => {
-        // Check if a user is signed in:
-        if (user) {
-            currentUser.collection("budget").get()
-                .then(allCategories => {
-                    allCategories.forEach(doc => {
-                        var categoryName = doc.data().name; //gets the name field
-
-                        var categoryPercentage = doc.data().percentage; //gets the length field
-                        let budgetRow = budgetTemplate.content.cloneNode(true);
-                        budgetRow.querySelector('.category-name').innerHTML = categoryName;   //equiv getElementByClassName
-                        budgetRow.querySelector('.category-percent').innerHTML = categoryPercentage;  //equiv getElementByClassName
-                        budgetGroup.appendChild(budgetRow);
-                    });
-                });
-            };
-    });
+    currentUser.collection("budget").get()
+        .then(allCategories => {
+            allCategories.forEach(doc => {
+                var categoryName = doc.data().name; //gets the name field
+                var categoryPercentage = doc.data().percentage; //gets the percentage field
+                let budgetRow = budgetTemplate.content.cloneNode(true);
+                budgetRow.querySelector('.category-name').innerHTML = categoryName;   //equiv getElementByClassName
+                budgetRow.querySelector('.category-percent').innerHTML = categoryPercentage;  //equiv getElementByClassName
+                budgetGroup.appendChild(budgetRow);
+            });
+        });
 }
-getCategories();
