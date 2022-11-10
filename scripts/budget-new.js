32be340bdf80db61b507afcfeae237a97eb3b756
Tyraffe <nathanb1357@gmail.com>
2022-11-10 12:52:11 -0800

    You can now delete categories in the budget creator.

--- scripts/budget-new.js
+++ scripts/budget-new.js
@@ -7,19 +7,19 @@ function addBudget(){
             currentUser = db.collection("users").doc(user.uid);
             currentUser.collection("categories").limit(1).get().then(sub => {
                 if (sub.docs.length == 0) {
-                    currentUser.collection("categories").doc("housing").set({
+                    currentUser.collection("categories").doc("Housing").set({
                         name: "Housing",
                         percentage: 0.35
                     }, {merge: true})    
-                    currentUser.collection("categories").doc("transportation").set({
+                    currentUser.collection("categories").doc("Transportation").set({
                         name: "Transportation",
                         percentage: 0.15
                     }, {merge: true})
-                    currentUser.collection("categories").doc("food").set({
+                    currentUser.collection("categories").doc("Food").set({
                         name: "Food",
                         percentage: 0.10
                     }, {merge: true})
-                    currentUser.collection("categories").doc("utilities").set({
+                    currentUser.collection("categories").doc("Utilities").set({
                         name: "Utilities",
                         percentage: 0.10
                     }, {merge: true})
@@ -55,8 +55,13 @@ function getCategories() {
         });
 }
 
-function deleteCategory() {
-    
+function deleteCategory(clicked) {
+    var selected = clicked.parentNode;
+    let documentName = selected.querySelector('.category-name').innerHTML;
+    currentUser.collection("categories").doc(documentName).delete().then(() => {
+        console.log("Document deleted!")
+    })
+    selected.remove();
 }
 
 
