76087410376ddfe9f3e4e819f10236a08c585025
Tyraffe <nathanb1357@gmail.com>
2022-11-23 12:27:49 -0800

    Started on the graph page

--- scripts/graphs.js
+++ scripts/graphs.js
@@ -1,13 +1,57 @@
 var currentUser;
+var incomeSum = 0;
+var userCurrency;
+var xValues = [];
+var yValues = [];
+var barColors = [];
 
 function checkLogin(){
     firebase.auth().onAuthStateChanged(user => {
         // Check if a user is signed in:
         if (user) {
             currentUser = db.collection("users").doc(user.uid);
+            username = user.displayName;
+            userCurrency = db.collection("currency").doc(currentUser.data().currency);
+            document.getElementById("name-goes-here").innerText = username;
+            getIncomeSum();
+            addExpected();
         } else {
             window.location.assign("index.html");
         }
     })
 }
 checkLogin();
+
+function getIncomeSum(){
+    currentUser.collection("income").get().then(allIncome => {
+        allIncome.forEach(doc => {
+            incomeSum = incomeSum + doc.data().income;
+        })
+    })
+}
+
+function addExpected(){
+    currentUser.collection("categories").get().then(allCategories => {
+        allCategories.forEach(doc => {
+            xValues.push(doc.data().name);
+            let y = doc.data().percentage * incomeSum;
+            let userCurrency;
+        })
+    })
+    new Chart("myChart", {
+        type: "pie",
+        data: {
+          labels: xValues,
+          datasets: [{
+            backgroundColor: barColors,
+            data: yValues
+          }]
+        },
+        options: {
+          title: {
+            display: true,
+            text: "World Wide Wine Production"
+          }
+        }
+      });
+}
