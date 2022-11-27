e50dff8623e0b5a52eff798f15948cdb30445d2f
bli129 <bennyli.byli@gmail.com>
2022-11-27 04:17:20 -0800

    cleaned up unused expense-editor.js code

--- scripts/expense-editor.js
+++ scripts/expense-editor.js
@@ -18,53 +18,9 @@ firebase.auth().onAuthStateChanged(user => {
     // Check if a user is signed in:
     if (user) {
         currentUser = db.collection("users").doc(user.uid);
-        // console.log(currentUser);
         displayCards();
-        currentUser.collection("expenses").get()
-        .then(allExpenses => {
-            allExpenses.forEach(doc => {
-                console.log(allExpenses);
-                uCurrencyType = doc.data().currencyType;
-                uExpense = doc.data().expense;
-                uCategory = doc.data().paymentCategory;
-                uPayType = doc.data().paymentType;
-                uDateAdded = doc.data().dateAdded;
-
-                let eachExpenseCurrency = document.createElement("input");
-                let eachExpenseExpense = document.createElement("input");
-                let eachExpenseCategory = document.createElement("input");
-                let eachExpenseType = document.createElement("input");
-                let eachExpenseDate = document.createElement("input");
-
-                eachExpenseCurrency.setAttribute("type", "text");
-                
-                // if(currencyType == null){
-                //     eachExpenseCurrency.setAttribute("value", "-");
-                // }else{
-                    eachExpenseCurrency.setAttribute("value", uCurrencyType);
-
-                // }
-                expenseContainer.appendChild(eachExpenseCurrency);
-
-                eachExpenseExpense.setAttribute("type", "text");
-                eachExpenseExpense.setAttribute("value", uExpense);
-                expenseContainer.appendChild(eachExpenseExpense);
-
-
-                eachExpenseCategory.setAttribute("value", uCategory);
-                eachExpenseCategory.setAttribute("text", "type");
-                expenseContainer.appendChild(eachExpenseCategory);
-
-                eachExpenseType.setAttribute("value", uPayType);
-                eachExpenseType.setAttribute("text","type");
-                expenseContainer.appendChild(eachExpenseType);
-
-                // eachExpenseDate.setAttribute("value", dateAdded);
-                // expenseContainer.appendChild(eachExpenseDate);
-
-                
-            })
-        })
+    } else {
+        window.location.assign("index.html");
     }
 })
 
